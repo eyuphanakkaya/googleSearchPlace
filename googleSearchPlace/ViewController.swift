c03d47590ed34dad8d9aa5d4d49fa354e26c78a4
Eyuphan Akkaya <akkayaeyupp@gmail.com>
2023-06-21 12:59:18 +0300

    Finished

--- googleSearchPlace/ViewController.swift
+++ googleSearchPlace/ViewController.swift
@@ -1,19 +1,56 @@
-//
-//  ViewController.swift
-//  googleSearchPlace
-//
-//  Created by Eyüphan Akkaya on 21.06.2023.
-//
-
 import UIKit
+import GoogleMaps
+import GooglePlaces
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchResultsUpdating {
+    
+    @IBOutlet weak var mapView: MKMapView!
 
+    let searchVC = UISearchController(searchResultsController: ResultViewController())
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchVC.searchResultsUpdater = self
+        navigationItem.searchController = searchVC
+        searchVC.searchBar.backgroundColor = .secondarySystemBackground
+       
     }
-
-
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text ,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty,
+              let resultsVC = searchController.searchResultsController as? ResultViewController else{
+                return
+              }
+        
+        resultsVC.delegate = self
+        
+        GooglePlacesManager.shared.findPlaces(query: query) { result in
+            switch result {
+            case .success(let places):
+                DispatchQueue.main.async {
+                    resultsVC.update(with: places)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
 }
+extension ViewController: ResultViewControllerDelegate {
+    func didTapPlace(with coordinate: CLLocationCoordinate2D) {
+        searchVC.searchBar.resignFirstResponder()
+        
+        
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        mapView.addAnnotation(pin)
+        mapView.setRegion(MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)), animated: true)
 
+    }
+    
+    
+}
